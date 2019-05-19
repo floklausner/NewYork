4964355a20e4a3d0d072cd3069585c9cd84f83d0
floklausner <klausnerflo@gmail.com>
2019-05-19 14:48:31 +0200

    commit

---
+++ script/contact.js
@@ -0,0 +1,54 @@
+'use strict'
+
+window.addEventListener('load', () => {
+  let navbar = document.getElementById('navbar');
+  let maxHeight = navbar.clientHeight;
+
+  navbar.style.display = 'none';
+
+  document.getElementById('menu').addEventListener('click', () => {
+    if (navbar.style.display === 'none') {
+        navbar.style.display = 'block';
+   } else {
+     navbar.style.display = 'none';
+   }
+  });
+
+document.getElementById('button').addEventListener('click', () => {
+  let firstname = document.getElementById('firstname');
+  let lastname = document.getElementById('lastname');
+  let email = document.getElementById('mail');
+  let textarea = document.getElementById('textfield');
+  let userFeedBack = document.getElementById('userfdb');
+
+  firstname.style.borderBottom = "0.0625rem solid #999";
+  lastname.style.borderBottom = "0.0625rem solid #999";
+  email.style.borderBottom = "0.0625rem solid #999";
+  textarea.style.borderBottom = "0.0625rem solid #999";
+  userfdb.textContent = "\u00A0";
+
+  if (firstname.value === "") {
+    firstname.style.borderBottom = "0.0625rem solid red"
+    userfdb.textContent = "Firstname can't be empty"
+  } else if (lastname.value === "") {
+    lastname.style.borderBottom = "0.0625rem solid red"
+    userfdb.textContent = "Lastname can't be empty"
+  } else if (email.value === "") {
+    email.style.borderBottom = "0.0625rem solid red"
+    userfdb.textContent = "Email can't be empty"
+  } else if (email.value !== "") {
+    if (!validateEmail(email)) {
+      email.style.borderBottom = "0.0625rem solid red"
+      userfdb.textContent = "Invalid Email"
+    }
+  } else if (textarea.value === "") {
+    textarea.style.borderBottom = "0.0625rem solid red"
+    userfdb.textContent = "Textarea can't be empty"
+  }
+});
+
+  function validateEmail(email) {
+    const splitEmail = email.value.split('@');
+    return splitEmail.length === 2 && splitEmail[1].split('.').length === 2 && splitEmail[1].split('.')[1].length >= 2;
+  }
+});
