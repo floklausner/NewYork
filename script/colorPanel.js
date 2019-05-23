4839877950fd2faf2b37b2d965ad50b1ce599f18
floklausner <klausnerflo@gmail.com>
2019-05-23 21:08:19 +0200

    commit

--- script/colorPanel.js
+++ script/colorPanel.js
@@ -17,7 +17,14 @@ window.addEventListener('load', () => {
           const heading  = document.getElementById('headline');
           const burger = document.getElementById('menu');
           const arrow = document.getElementById('arrowDown');
+          const gallery = document.getElementById('navGallery');
+          const subway = document.getElementById('navSubway');
+          const taxi = document.getElementById('navTaxi');
+          const times = document.getElementById('navTimes');
+          const contact = document.getElementById('navContact');
+          const about = document.getElementById('navAbout');
 
+          // Heading
           heading.addEventListener('mouseover', () => {
             heading.style.color = eintragData[i];
           });
@@ -26,6 +33,7 @@ window.addEventListener('load', () => {
             heading.style.color = 'black';
           });
 
+          // Burger
           burger.addEventListener('mouseover', () => {
             burger.style.color = eintragData[i];
           });
@@ -34,12 +42,67 @@ window.addEventListener('load', () => {
             burger.style.color = 'black';
           });
 
+          // Arrow
           arrow.addEventListener('mouseover', () => {
             arrow.style.color = eintragData[i];
           });
 
           arrow.addEventListener('mouseout', () => {
             arrow.style.color = 'black';
+          });
+
+          // Gallery
+          navGallery.addEventListener('mouseover', () => {
+            navGallery.style.color = eintragData[i];
+          });
+
+          navGallery.addEventListener('mouseout', () => {
+            navGallery.style.color = 'black';
+          });
+
+          // Subway
+          navSubway.addEventListener('mouseover', () => {
+            navSubway.style.color = eintragData[i];
+          });
+
+          navSubway.addEventListener('mouseout', () => {
+            navSubway.style.color = 'black';
+          });
+
+          // Taxi
+          navTaxi.addEventListener('mouseover', () => {
+            navTaxi.style.color = eintragData[i];
+          });
+
+          navTaxi.addEventListener('mouseout', () => {
+            navTaxi.style.color = 'black';
+          });
+
+          // Times
+          navTimes.addEventListener('mouseover', () => {
+            navTimes.style.color = eintragData[i];
+          });
+
+          navTimes.addEventListener('mouseout', () => {
+            navTimes.style.color = 'black';
+          });
+
+          // Contact
+          navContact.addEventListener('mouseover', () => {
+            navContact.style.color = eintragData[i];
+          });
+
+          navContact.addEventListener('mouseout', () => {
+            navContact.style.color = 'black';
+          });
+
+          // About
+          navAbout.addEventListener('mouseover', () => {
+            navAbout.style.color = eintragData[i];
+          });
+
+          navAbout.addEventListener('mouseout', () => {
+            navAbout.style.color = 'black';
           });
 
         });
